6fea6065de51761c84dda2a6bfc08e89698c5ddb
eltrach <49155522+eltrach@users.noreply.github.com>
2022-10-29 14:38:20 +0100

    family

--- Test Sync/Assets/_Scripts/_Debug.cs
+++ Test Sync/Assets/_Scripts/_Debug.cs
@@ -7,7 +7,12 @@ public class _Debug : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.LogWarning("<color=red> hello ! </color> :) ") ;
+        Debug.LogWarning("<color=red> We are familly  ! </color> :) ") ;
+        Debug.LogWarning("<color=red> We are familly  ! </color> :) ") ;
+        Debug.LogWarning("<color=red> We are familly  ! </color> :) ") ;
+        Debug.LogWarning("<color=red> We are familly  ! </color> :) ") ;
+        Debug.LogWarning("<color=red> We are familly  ! </color> :) ") ;
+        Debug.LogWarning("<color=red> We are familly  ! </color> :) ") ;
     }
 
  
