2989d8a25be6d278f61c80699e255b8f4eaa0421
eltrach <49155522+eltrach@users.noreply.github.com>
2022-10-29 14:23:32 +0100

    hello :)
    GitHub test . exe

---
+++ Test Sync/Assets/_Scripts/_Debug.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Debug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogWarning("<color=red> hello ! </color> :) ") ;
+    }
+
+ 
+}
